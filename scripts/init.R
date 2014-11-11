142b2d2d0c81e33a336f35be4becc70c6ec7edbf
jolson7168 <technology@nododos.com>
2014-11-11 09:15:03 -0600

    backed out previous change

--- scripts/init.R
+++ scripts/init.R
@@ -8,5 +8,4 @@ install.packages("abrem",repos="http://R-Forge.R-project.org")
 install.packages("survival",repos="http://cran.r-project.org")
 install.packages("jsonlite",repos="http://cran.r-project.org")
 install.packages("httpuv",repos="http://cran.r-project.org")
-#Start the service
-Rscript /home/ec2-user/git/weibull/src/listener.R > /home/ec2-user/git/weibull/scripts/r.out &
+
